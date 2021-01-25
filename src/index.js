5b6315f93016554e501164173ae1cd2eeed1b7a5
Samoth112 <tc.thomascho@gmail.com>
2021-01-25 10:34:42 -0500

    index.js: setup firebase config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB_uDbHtybqtLGld2Z_D9lw7-wZnerutV8",
+  authDomain: "biblio-lite-15027.firebaseapp.com",
+  databaseURL: "https://biblio-lite-15027-default-rtdb.firebaseio.com",
+  projectId: "biblio-lite-15027",
+  storageBucket: "biblio-lite-15027.appspot.com",
+  messagingSenderId: "316410964557",
+  appId: "1:316410964557:web:10afa7b3711ccb719c0a04",
+  measurementId: "G-C5LDJY4KCJ"
+};
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
