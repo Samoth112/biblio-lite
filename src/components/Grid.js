378c764190ede96945c40992de17ddc253409168
Samoth112 <tc.thomascho@gmail.com>
2021-02-02 18:47:26 -0500

    revised gridProps down to three: grid-template, grid-auto-rows/columns, gap

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 export default function Grid(props) {
   return(
-    <section className={`${props.className}
+    // gridProps values should be class names
+    // that define the grid-template,
+    // grid-auto-rows, grid-auto-columns, and
+    // the gap css grid properties.
+    // these are the properties that currently
+    // define grids.
+    <section className={`${props.className} grid
       ${props.gridProps.gridTemp}
-      ${props.gridProps.gridCols}
-      ${props.gridProps.gridRows}
-      ${props.gridProps.colGap}
-      ${props.gridProps.rowGap}`
+      ${props.gridProps.gridAuto}
+      ${props.gridProps.gap}`
     }>
       {props.children}
     </section>
