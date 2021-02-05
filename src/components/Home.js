e979dec0c132e2ce592be2c45e72398043c73004
Samoth112 <tc.thomascho@gmail.com>
2021-02-05 15:40:36 -0500

    Home.js: removed gridProps and gridItemProps

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,20 +7,10 @@ export default class Home extends Component {
   render() {
     return(
       // refer to Grid.js to see full list of gridProps
-      <Grid className="home" 
-        gridProps={{
-          gridAuto: "grid-auto-rows-max-content"
-        }}
-      >
-        <Header className="header" 
-          gridProps={{
-            gridAuto: "grid-auto-rows-max-content-1fr"
-          }}
-        >
-          <GridItem gridItemProps={{
-            gridRow: "row1",
-            gridCol: "col1"  
-          }}>
+      <Grid className="home" gridAuto="grid-auto-rows-max-content">
+        <Header className="header" gridAuto="grid-auto-rows-max-content-1fr">
+          <GridItem gridRow="row1" gridCol= "col1">
+            
           </GridItem>
         </Header>
       </Grid>
