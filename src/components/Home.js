91183e8cc57ffbd374289c6d97b4465f695a5a8e
Samoth112 <tc.thomascho@gmail.com>
2021-02-05 15:38:08 -0500

    Home.js: imported Header.js and GridItem.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,28 @@
 import React, {Component} from 'react';
 import Grid from './Grid';
+import GridItem from './GridItem';
+import Header from './Header';
 
 export default class Home extends Component {
   render() {
     return(
       // refer to Grid.js to see full list of gridProps
-      <Grid className="home" gridProps={{
-        gridAuto: "grid-auto-rows-max-content"
-      }}>
-
+      <Grid className="home" 
+        gridProps={{
+          gridAuto: "grid-auto-rows-max-content"
+        }}
+      >
+        <Header className="header" 
+          gridProps={{
+            gridAuto: "grid-auto-rows-max-content-1fr"
+          }}
+        >
+          <GridItem gridItemProps={{
+            gridRow: "row1",
+            gridCol: "col1"  
+          }}>
+          </GridItem>
+        </Header>
       </Grid>
     );
   };
