6372795c1a88e4067e26763ca030b98e2b656e4e
Samoth112 <tc.thomascho@gmail.com>
2021-02-05 14:11:00 -0500

    Header.js: removed gridProps, added props.children

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,8 @@ import Grid from './Grid'
 export default function Header(props) {
   return(
     // refer to Grid.js to see full list of gridProps
-    <Grid className='header' gridProps={{
-      gridAuto: 'grid-auto-rows-max-content-1fr'
-    }}>
-
+    <Grid className='header' gridAuto='grid-auto-rows-max-content-1fr'>
+      {props.children}
     </Grid>
   );
 };
