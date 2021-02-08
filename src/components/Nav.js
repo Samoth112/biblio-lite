c408f73a2a26f3465f55221502af1c48139fe47c
Samoth112 <tc.thomascho@gmail.com>
2021-02-08 09:58:57 -0500

    Nav.js:  now defined by Grid component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Grid from './Grid';
 
 export default function Nav(props) {
+  // Unlike Header.js, there may be multiple types of Nav components that
+  // are defined by a class name of "header__nav" or "footer__nav," for example.
+  // Also unlike Header.js and Home.js, the className prop should not populated
+  // with a className that reflects the component name, like "nav." Instead,
+  // use props.ClassName, which will be something like "header__nav" or "footer__nav."
   return(
-    <nav className={`nav ${props.className}`}>
-      {props.children}
-    </nav>
+    <Grid className={props.className} gridTemp={props.gridTemp} gridAuto={props.gridAuto} gridGap={props.gridGap}>
+
+    </Grid>
   );
 };
