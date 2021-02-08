3332dcdec204032dd46eb94014445460ab37286a
Samoth112 <tc.thomascho@gmail.com>
2021-02-08 10:03:54 -0500

    Grid.js: converted to class component  to render either a header, nav, or section HTML element depending on class name

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default function Grid(props) {
-  return(
+export default class Grid extends Component {
+  render() {
+    let grid;
     // Prop values should be class names that define the grid-template,
     // grid-auto-rows/columns, and gap css grid properties. These are 
     // the properties that currently define grid containers.
     // Nested Grid components with no other grid items
     // between them should not be wrapped in GridItem components.
-    <section className={`${props.className} ${props.gridTemp} ${props.gridAuto} ${props.gridGap}`}>
-      {props.children}
-    </section>            
-  );
+    if(this.props.className === 'header') {
+      grid = 
+      <header className={`${this.props.className} ${this.props.gridTemp} ${this.props.gridAuto} ${this.props.gridGap}`}>      
+        {this.props.children}
+      </header> 
+    } else if(this.props.className.includes('nav')){
+      grid =
+      <nav className={`${this.props.className} ${this.props.gridTemp} ${this.props.gridAuto} ${this.props.gridGap}`}>      
+        {this.props.children}
+      </nav>
+    } else {
+      grid =
+      <section className={`${this.props.className} ${this.props.gridTemp} ${this.props.gridAuto} ${this.props.gridGap}`}>      
+        {this.props.children}
+      </section>
+    }
+    
+    return(
+      grid           
+    );
+  };
 };
 
 Grid.defaultProps = {
