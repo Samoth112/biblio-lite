795f8b90e946cd725a31cd11f692e303e2d45170
Samoth112 <tc.thomascho@gmail.com>
2021-05-30 10:32:15 -0400

    added explanatory modal to Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,11 @@ import Header from './Header';
 import Nav from './Nav';
 import AddressSearchForm from '../components/AddressSearchForm';
 
-export default function Home(): React.ReactElement {
+export default function Home(): React.ReactElement {  
+  const closeModal = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.currentTarget.style.display = 'none';
+  };
   return(
     <section className='home'>
       <Grid className="home__grid">
@@ -22,7 +26,22 @@ export default function Home(): React.ReactElement {
               <AddressSearchForm />
             </Grid>
           </section>
+          
         </Header>
+        <div id="home__modal" onClick={closeModal}>
+          <div className="home__modal-content">
+            <span className="home__modal---close">&times;</span>
+            <p>
+              Hello, and thanks for checking out Biblio, a project to help promote book access in the community.
+              Biblio taps into the existing Little Free Library network and builds upon it by allowing users to sponser
+              their local Little Free Libraries, expanding its offering.
+            </p>
+            <p>
+              This is currently a demo with a limited dataset. All searches will, for the time being, be limited
+              to the current available area. More Little Free Libraries to come! 
+            </p>
+          </div>
+        </div>
       </Grid>
     </section>
   );
