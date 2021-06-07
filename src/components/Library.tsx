9278f5168fa8154c8ac1788a74e81c9319f8122d
Samoth112 <tc.thomascho@gmail.com>
2021-06-07 19:24:07 -0400

    debug

--- src/components/Library.tsx
+++ src/components/Library.tsx
@@ -9,6 +9,7 @@ import LibraryBooksNew from './LibraryBooksNew';
 import LibraryCard from './LibraryCard';
 import SponserBook from './SponserBook';
 import SponserBooks from './SponserBooks';
+
 // component rendered through Route render prop takes MatchParams
 interface MatchParams {
   id: string;
@@ -18,7 +19,11 @@ export default function Library({match}: RouteComponentProps<MatchParams>): Reac
   let name = "";
   let dewey_style_charter = "";
   const lib = useSelector((state: AppState) => state.results.selectedLib);
-  const redirect = useSelector((state: AppState) => state.libraryBooks.redirect)
+  const redirect = useSelector((state: AppState) => state.libraryBooks.redirect);
+  debugger;
+  // the redirect property is set in the libraryBooksReducer. The LibraryBooks component will
+  // dispatch the SET_LIBRARY_BOOKS action, which sets redirect to true.
+  // If true, 
   const dispatch = useDispatch();
   const getLibrary = () => {
     fetch(`https://lite-api.herokuapp.com/little_libraries/${match.params.id}`, {
@@ -58,12 +63,12 @@ export default function Library({match}: RouteComponentProps<MatchParams>): Reac
         <LibraryCard className="library__library-card--no-border" grid="library__library-card-grid" id={lib.id} charter={dewey_style_charter} name={name} sponsers={lib.sponsers} />
         <Switch>
           <Route path="/results/little_libraries/:littleLibraryId/sponsers/:sponserId/sponser_books/:id" render={(props) => <SponserBook {...props} /> } />
+          <Route path="/results/little_libraries/:littleLibraryId/sponsers/:id" render={(props) => <SponserBooks {...props} />} />
           {redirect ? 
             <Redirect to={`/results/little_libraries/${match.params.id}`} />
           :
             <Route path="/results/little_libraries/:littleLibraryId/library_books/new" render={(props) => <LibraryBooksNew {...props} /> } />
           }
-          <Route path="/results/little_libraries/:littleLibraryId/sponsers/:id" render={(props) => <SponserBooks {...props} />} />
           <Route path="/results/little_libraries/:littleLibraryId/library_books/:id" render={(props) => <LibraryBook {...props} />} />
           <Route path="/results/little_libraries/:id" render={(props) => <LibraryBooks {...props} />} />
         </Switch>
